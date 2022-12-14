28e3e890a296db1aa98c04319ec500788cb5c534
Piotr Domański <pi.domanski@gmail.com>
2022-12-14 22:43:51 +0100

    allow logging to stdout

--- catgan/generate.py
+++ catgan/generate.py
@@ -3,6 +3,23 @@ from .train import batch_of_noise
 from math import sqrt, ceil
 import torchvision.transforms as T
 import matplotlib.pyplot as plt
+from typing import List, Any
+
+
+def generate_cat_images(generator: LSGANGenerator, amount: int) -> List[Any]:
+    """generate.
+
+    :param generator:
+    :type generator: LSGANGenerator
+    :param amount:
+    :type amount: int
+    :rtype: List[Any]
+    """
+    noise = batch_of_noise(amount, generator.in_features)
+    cat_images = generator(noise).cpu()
+    transform = T.ToPILImage()
+    cat_images = [transform(cat_image).convert("RGB") for cat_image in cat_images]
+    return cat_images
 
 
 def generate(generator: LSGANGenerator, amount: int) -> None:
@@ -14,20 +31,15 @@ def generate(generator: LSGANGenerator, amount: int) -> None:
     :type amount: int
     :rtype: None
     """
-    noise = batch_of_noise(amount, generator.in_features)
-    cat_images = generator(noise).cpu()
-    transform = T.ToPILImage()
-
+    cat_images = generate_cat_images(generator, amount)
     rows = int(sqrt(amount))
     cols = ceil(amount / rows)
-    fig, ax = plt.subplots(rows, cols)
+    _, ax = plt.subplots(rows, cols)
 
     for row in range(rows):
         for col in range(cols):
             try:
-                ax[row][col].imshow(
-                    transform(cat_images[cols * row + col]).convert("RGB")
-                )
+                ax[row][col].imshow(cat_images[cols * row + col])
             except IndexError:
                 break
 
